6bb398dcaf0e5bfec6a5cb3b2e29622b02c9925e
firatsertgoz <firatsertgoz@sabanciuniv.edu>
2024-01-09 01:51:45 +0100

    Start programmatic body construction

--- throw/src/control/body.rs
+++ throw/src/control/body.rs
@@ -1,16 +1,10 @@
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
+use bevy_rapier3d::{prelude::*, rapier::crossbeam::epoch::Pointable};
+
+use crate::camera::CameraTrackable;
 
 #[derive(Debug, Component)]
 pub struct Body;
-
-pub struct BodyPlugin;
-
-impl Plugin for BodyPlugin {
-    fn build(&self, app: &mut App) {
-        //  app.add_systems(Startup, setup_spine);
-    }
-}
 
 fn setup_body(
     mut commands: Commands,
@@ -45,13 +39,7 @@ fn setup_right_leg(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
 }
-
-fn setup_arms(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-}
+//TODO: Add Spherical Joint to connect the arm to the torso
 
 fn setup_left_arm(
     mut commands: Commands,
@@ -59,17 +47,84 @@ fn setup_left_arm(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
 }
+
+//TODO: Add Spherical Joint to connect the arm to the torso
 fn setup_right_arm(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    commands.spawn(RigidBody::Dynamic).insert((
+        Collider::capsule(
+            Vec3 {
+                x: 0.0,
+                y: 2.0,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            0.25,
+        ),
+        CameraTrackable,
+    ));
 }
 
-fn setup_spine(
+pub(crate) fn setup_spine(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    
+    commands.spawn(RigidBody::Dynamic).insert((
+        Collider::capsule(
+            Vec3 {
+                x: 0.0,
+                y: 2.0,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            0.25,
+        ),
+        CameraTrackable,
+    ));
+}
+
+pub(crate) fn setup_torso(mut commands: Commands) {
+    // Attach a single collider to a rigid-body.
+    commands.spawn(RigidBody::Dynamic).insert((
+        Collider::capsule(
+            Vec3 {
+                x: 0.0,
+                y: 3.0,
+                z: 0.0,
+            },
+            Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            1.0,
+        ),
+        CameraTrackable,
+    ));
+
+    // Attach a multiple colliders to a rigid-body.
+    // commands
+    //     .spawn(RigidBody::Dynamic)
+    //     .with_children(|children| {
+    //         children
+    //             .spawn(Collider::ball(0.5))
+    //             // Position the collider relative to the rigid-body.
+    //             .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, -1.0)));
+    //         children
+    //             .spawn(Collider::ball(0.5))
+    //             // Position the collider relative to the rigid-body.
+    //             .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 1.0)));
+    //     });
 }
--- throw/src/control/mod.rs
+++ throw/src/control/mod.rs
@@ -1,5 +1,9 @@
+mod body;
+
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
+
+use self::body::{setup_torso, setup_spine};
 
 #[derive(Debug, Component)]
 pub struct Body;
@@ -8,6 +12,7 @@ pub struct BodyPlugin;
 
 impl Plugin for BodyPlugin {
     fn build(&self, app: &mut App) {
-        //  app.add_systems(Startup, setup_spine);
+        app.add_systems(Startup, setup_torso);
+        app.add_systems(Startup, setup_spine);
     }
 }
